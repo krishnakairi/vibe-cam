07b5872a433f85bf4f626e21eb59e77d16fe338c
Krishna Kairi <krishna.kairi@outlook.com>
2022-07-10 01:29:16 +0530

    site: comment webcam component

--- site/src/pages/Track.tsx
+++ site/src/pages/Track.tsx
@@ -1,7 +1,7 @@
 import React, { useEffect } from 'react';
 import { useParams } from 'react-router-dom';
 import useTrackStore from '../store/track';
-import Webcam from "react-webcam";
+// import Webcam from "react-webcam";
 
 const videoConstraints = {
   width: 1280,
@@ -14,17 +14,27 @@ function Track() {
   const connection = useTrackStore((state: any) => state.connection);
   const createConnection = useTrackStore((state: any) => state.createConnection);
 
+  useEffect(() => {
+    navigator.mediaDevices.getUserMedia({
+      video: true,
+      audio: true
+    }).then((stream) => {
+      console.log(stream)
+      createConnection(id, stream);
+    }).catch(() => {})
+  }, [])
+
   return (
     <div>
       <h2>Track</h2>
       <p>{connection.id} - {connection.status}</p>
-      <Webcam
+      {/* <Webcam
         audio={false}
         videoConstraints={videoConstraints}
         onUserMedia={ (stream) => {
           createConnection(id, stream);
         }}
-      />
+      /> */}
     </div>
   );
 }
