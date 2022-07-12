b91e759b426b35d267b8a9daad64d338c1391034
Krishna Kairi <krishna.kairi@outlook.com>
2022-07-12 13:17:24 +0530

    api: fix signal 500 error

--- site/pages/api/signal.js
+++ site/pages/api/signal.js
@@ -1,11 +1,10 @@
 // Next.js API route support: https://nextjs.org/docs/api-routes/introduction
 const Pusher = require("pusher");
 
-export default function handler(req, res) {
-
+export default async function handler(req, res) {
   if (req.method === 'POST') {
-
     const { connectionId, signal, peerId } = req.body
+    console.log(`vibe-cam-${connectionId}`, peerId, JSON.stringify(signal))
 
     const pusher = new Pusher({
       appId: process.env.PUSHER_APP_ID,
@@ -19,11 +18,10 @@ export default function handler(req, res) {
      * todo
      * validate req.body, check for XSS attack
      */
-    console.log(`vibe-cam-${connectionId}`, JSON.stringify(signal))
-    pusher.trigger(`vibe-cam-${connectionId}`, "signal", { peerId, signal });
+    await pusher.trigger(`vibe-cam-${connectionId}`, "signal", { peerId, signal });
   
     res.status(200).json({ status: true, error: null })
+  } else {
+    res.status(400).json({ status: false, error: 'ivalid route'});
   }
-
-  res.status(400).json({ status: false, error: 'ivalid route'});
 }
